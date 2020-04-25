5a66fbb6c65ef2971de11ef9bdd22631c175fe2b
drokk <drokkie@gmail.com>
2020-04-25 13:41:38 -0700

    start on file support

--- python/doppler.py
+++ python/doppler.py
@@ -1,4 +1,6 @@
 #!/usr/local/bin/python3
+#TODO switch to hashlib
+#TODO flesh out file digestion
 from Crypto.Hash import *
 import argparse 
 
@@ -6,18 +8,18 @@ import argparse
 #available_hashes = Crypto.Hash.__all__
 
 parser = argparse.ArgumentParser(description='lets create some hashes')
-parser.add_argument(metavar='String', type=str, nargs='+',dest='input', help='give me word')
+parser.add_argument('--strings', type=str, nargs='+',dest='input', help='give me word')
 parser.add_argument('--hash', dest='hash2', help='valid options are SHA256, MD5, SHA1' )
-
+parser.add_argument('--file', dest='file', help='file name')
 args = parser.parse_args()
 
-print(args.input,' ',args.hash2)
+#print(args.input,' ',args.hash2)
 
 
 # offloading argument inputs into local variables 
 args_input = args.input  
 args_hashes = args.hash2.upper()  
-
+args_file = args.file 
 # catch the all in the hashes option 
 if args_hashes == 'ALL':
     args_hashes = 'SHA256,MD5,SHA1'
@@ -28,17 +30,17 @@ if args_hashes.find(','): # if receiving a string of comma separated options spl
 else:
     hashes = args_hashes
 
+if args_input : # only what to do checksum on cli input if there not files 
+    for string in args_input:
+        for hash2 in hashes: 
+            if hash2 == 'SHA256':
+                hash_object = SHA256.new(data=string.encode())
+            elif hash2 == 'MD5':
+                    hash_object = MD5.new(data=string.encode())
+            elif hash2 == 'SHA1':
+                    hash_object = SHA1.new(data=string.encode())
+            else:
+                print('unknown hash')     
 
-for string in args_input:
-    for hash2 in hashes: 
-        if hash2 == 'SHA256':
-            hash_object = SHA256.new(data=string.encode())
-        elif hash2 == 'MD5':
-                hash_object = MD5.new(data=string.encode())
-        elif hash2 == 'SHA1':
-                hash_object = SHA1.new(data=string.encode())
-        else:
-            print('unknown hash')     
-
-        if 'hash_object' in locals():     # if the hash_object is defined print out the string,hash type and the hash
-            print(string,' ',hash2,' ',hash_object.hexdigest())
+            if 'hash_object' in locals():     # if the hash_object is defined print out the string,hash type and the hash
+                print(string,' ',hash2,' ',hash_object.hexdigest())
