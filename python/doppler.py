2777e604b7bfdbc79ebac9d751682af921153af7
drokk <drokkie@gmail.com>
2020-04-26 09:21:34 -0700

    now supports several files as input

--- python/doppler.py
+++ python/doppler.py
@@ -9,7 +9,7 @@ import argparse
 parser = argparse.ArgumentParser(description='lets create some hashes')
 parser.add_argument('--strings', type=str, nargs='+',dest='input', help='give me word')
 parser.add_argument('--hash', dest='hash2', help='valid options are SHA256, MD5, SHA1' )
-parser.add_argument('--file', dest='file', help='file name')
+parser.add_argument('--file', type=str, dest='files', nargs='+', help='file name(s)')
 args = parser.parse_args()
 
 #print(args.input,' ',args.hash2)
@@ -18,8 +18,8 @@ args = parser.parse_args()
 # offloading argument inputs into local variables 
 args_input = args.input  
 args_hashes = args.hash2.lower()  
-args_file = args.file 
-BLOCK_SIZE = 65536
+args_files = args.files 
+BLOCK_SIZE = 65536 #increase this value if want to back file hashing with more memory for speed reasons 
 # catch the all in the hashes option 
 if args_hashes == 'all':
     args_hashes = 'sha256,md5,sha1'
@@ -40,13 +40,14 @@ if args_input : # only what to do checksum on cli input if there not files
             if 'hashobject' in locals():     # if the hash_object is defined print out the string,hash type and the hash
                 print(string,' ',hashtype,' ',hashobject.hexdigest())
 
-if args_file : #read the file and produce the hash, see https://nitratine.net/blog/post/how-to-hash-files-in-python/
-    for hashtype in hashes:
-        hashobject = hashlib.new(hashtype) 
-        with open(args_file, 'rb') as file: 
-            file_block = file.read(BLOCK_SIZE)
-            while len(file_block) > 0:
-                hashobject.update(file_block)
-                file_block = file.read(BLOCK_SIZE)
-        print(args_file,' | ',hashtype,' | ',hashobject.hexdigest())        
+if args_files : #read a file and produce the hash, see https://nitratine.net/blog/post/how-to-hash-files-in-python/
+    for file in args_files: 
+        for hashtype in hashes:
+            hashobject = hashlib.new(hashtype) 
+            with open(file, 'rb') as file_handle: 
+                file_block = file_handle.read(BLOCK_SIZE)
+                while len(file_block) > 0:
+                    hashobject.update(file_block)
+                    file_block = file_handle.read(BLOCK_SIZE)
+            print(file,' | ',hashtype,' | ',hashobject.hexdigest())        
 
